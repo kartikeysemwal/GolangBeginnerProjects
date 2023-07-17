5e160efc78630d8b7799e3a7c2cb414593baa158
Kartikey Semwal <semwal.kartikey999@gmail.com>
2023-07-17 22:34:07 +0530

    video chat WebRTC

---
+++ VideoChatWebRTC/client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import CreateRoom from './components/CreateRoom'
+import Room from './components/Room'
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={CreateRoom}></Route>
+          <Route path="/room/:roomID" Component={Room}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ VideoChatWebRTC/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"VideoChatWebRTC/server"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	server.AllRooms.Init()
+
+	http.HandleFunc("/create", server.CreateRoomRequestHandler)
+	http.HandleFunc("/join", server.JoinRoomRequestHandler)
+
+	log.Println("Starting server on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
